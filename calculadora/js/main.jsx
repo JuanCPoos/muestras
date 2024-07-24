15290129579fba0cdd3a5d9d35790392e29298b0
JuanCPoos <juanpoos.e@gmail.com>
2024-07-24 15:54:06 -0300

    [ADD] Nuevo ejericio, calculadora

---
+++ calculadora/js/main.jsx
@@ -0,0 +1,12 @@
+/* en este archivo irá toda la lógica para 
+realizar las operaciones aritméticas */
+
+/* recibir los valores del DOM */
+const input1 = document.getElementById('input1');
+
+
+const suma = (a, b) => a + b;
+const resta = (a, b) => a - b;
+const multiplicar = (a, b) => a * b
+const dividir = (a, b) => a / b
+
